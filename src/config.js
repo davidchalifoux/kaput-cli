5f29f79e05856372c5090a3427057f9389a63011
David Chalifoux <dccfoux@gmail.com>
2020-07-09 16:28:43 -0500

    Version bump and update migration

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const Conf = require('conf')
 const config = new Conf({migrations: {
-  '0.0.1': store => {
-    store.set('version', '0.0.1')
+  '0.0.2': store => {
+    store.set('version', '0.0.2')
     // Remove authCodes, they are one time use only
     if (store.has('authCode')) {
       store.delete('authCode')
